b6f2431aaeebb6a9eccba6f65ae2952b5efb7cbf
Mohamad Al Sayadi <muhammed.alsayadi@gmail.com>
2018-08-26 16:02:09 +0400

    Use md5

--- .circleci/decrypt.sh
+++ .circleci/decrypt.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get install libressl
-
-libressl aes-256-cbc -d -in secret-env-cipher -out secret-env-plain -k ${OPENSSL_KEY}
+openssl aes-256-cbc -d -in secret-env-cipher -out secret-env-plain -md md5 -pass env:OPENSSL_PASSWORD
 source secret-env-plain
 #rm secret-env-plain
 
