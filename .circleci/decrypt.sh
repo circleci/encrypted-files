1a94a0f7d61f2df42e929dd7c96dd2052c2a0b66
Mohamad Al Sayadi <muhammed.alsayadi@gmail.com>
2018-08-26 15:31:12 +0400

    Install libressl from apt-get

--- .circleci/decrypt.sh
+++ .circleci/decrypt.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/libressl-portable/portable.git libressl
-cd libressl && sh autogen.sh
-./configure --prefix="${HOME}/opt/libressl"
-make
-sudo make install
-echo '/opt/libressl' | sudo tee /etc/ld.so.conf.d/libressl.conf
-sudo ldconfig
-cd ..
+sudo apt-get install libressl
 
-openssl aes-256-cbc -d -in secret-env-cipher -out secret-env-plain -k ${OPENSSL_KEY}
+libressl aes-256-cbc -d -in secret-env-cipher -out secret-env-plain -k ${OPENSSL_KEY}
 source secret-env-plain
 #rm secret-env-plain
 
