5e3dcf537b894cbbeddd3846ae8ad862d2d0570b
Mohamad Al Sayadi <muhammed.alsayadi@gmail.com>
2018-08-26 14:47:05 +0400

    Install latest openssl

--- .circleci/decrypt.sh
+++ .circleci/decrypt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y openssl-2.7.7
+sudo apt-get install -y openssl
 openssl aes-256-cbc -d -in secret-env-cipher -out secret-env-plain -k ${OPENSSL_KEY}
 source secret-env-plain
 #rm secret-env-plain
