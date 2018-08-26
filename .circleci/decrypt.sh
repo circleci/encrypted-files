17f53cc45fe6598ae2987fedb8f4dcc5818839d2
Mohamad Al Sayadi <muhammed.alsayadi@gmail.com>
2018-08-26 16:13:11 +0400

    Use sha256

--- .circleci/decrypt.sh
+++ .circleci/decrypt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-openssl aes-256-cbc -d -in secret-env-cipher -out secret-env-plain -md md5 -pass env:OPENSSL_PASSWORD
+openssl aes-256-cbc -d -in secret-env-cipher -out secret-env-plain -md sha256 -pass env:OPENSSL_PASSWORD
 source secret-env-plain
 #rm secret-env-plain
 
