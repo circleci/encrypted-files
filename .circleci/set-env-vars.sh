9a87fd19fc6da0df2b77b424ed6371fd647e9089
Mohamad Al Sayadi <muhammed.alsayadi@gmail.com>
2018-08-27 08:43:58 +0400

    Only export to bash env

--- .circleci/set-env-vars.sh
+++ .circleci/set-env-vars.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-openssl aes-256-cbc -d -in secret-env-cipher -out secret-env-generated -md sha256 \
+openssl aes-256-cbc -d -in secret-env-cipher -md sha256 \
     -pass env:OPENSSL_PASSWORD >> $BASH_ENV
-
-source secret-env-generated
-
-rm secret-env-generated
 
 echo "FOO: ${FOO} BAR: ${BAR}"
