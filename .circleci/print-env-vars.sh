2d5eaebd93faed51f0f78669878a33ea0c6f30c4
Mohamad Al Sayadi <muhammed.alsayadi@gmail.com>
2018-08-27 23:28:21 +0400

    Add warning comments in print step

--- .circleci/print-env-vars.sh
+++ .circleci/print-env-vars.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+# Printing the values of the env vars for testing and verification.
+# Do not do this in a production setup!
+# Do not use actual sensitive data for testing!
 echo "FOO: ${FOO} BAR: ${BAR}"
